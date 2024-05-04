9bdb05bdb6d73849ce39a5c8feab65800b47ce12
ABISHCHHETRI <165697751+ABISHCHHETRI@users.noreply.github.com>
2024-05-04 22:42:14 +0530

    new

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Board from './Pages/Board.js'
 import Home from './Pages/Home.js'
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://maya-anjz.onrender.com'; 
+axios.defaults.baseURL = 'https://puppy-mzmq.onrender.com'; 
 // axios.defaults.baseURL = 'http://localhost:8000';
 function App() {
   return (
