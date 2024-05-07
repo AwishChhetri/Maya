b1f54b9dc396fe0f3df5ea4df7eb88f0109a82b0
ABISHCHHETRI <165697751+ABISHCHHETRI@users.noreply.github.com>
2024-05-08 00:07:53 +0530

    the messages fetch function added

--- src/Pages/Chat.js
+++ src/Pages/Chat.js
@@ -1,37 +1,40 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import { Box, Text, Input, Button, Flex, List, ListItem, Spinner } from '@chakra-ui/react';
 
 const Chat = ({ senderId, receiverId, receiverName, roomId }) => {
   const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState('');
-  const [isLoading, setIsLoading] = useState(false); // State to track loading status
-  const socket = io('https://puppy-mzmq.onrender.com');
+  const [isLoading, setIsLoading] = useState(false);
+  const [socket, setSocket] = useState(null);
+  
+  useEffect(() => {
+    const newSocket = io('https://puppy-mzmq.onrender.com');
+    setSocket(newSocket);
+
+    return () => newSocket.disconnect();
+  }, []);
 
   useEffect(() => {
+    if (!socket) return;
+
+    socket.emit('connected', roomId);
+
+    socket.on('previous_messages', (msg) => {
+      setMessages(msg);
+    });
+
     socket.on('chat message', (msg) => {
-      setMessages((prevMessages) => [...prevMessages, msg]);
+      if (msg.content.trim() !== '') { // Check if the message content is not empty
+        setMessages((prevMessages) => [...prevMessages, msg]);
+      }
     });
 
     return () => {
-      socket.disconnect();
+      socket.off('previous_messages');
+      socket.off('chat message');
     };
-  }, [socket, roomId]); 
-
-  const fetchMessages = useCallback(async () => {
-    try {
-      const response = await axios.get(`/chat-room-messages?roomId=${roomId}`);
-      const { messages } = response.data;
-      setMessages(messages);
-    } catch (error) {
-      console.error('Error fetching messages:', error);
-    }
-  }, [axios, roomId]);
-
-  useEffect(() => {
-    fetchMessages();
-  }, [fetchMessages]);
+  }, [socket, roomId]);
 
   const handleInputChange = (event) => {
     setInputMessage(event.target.value);
@@ -40,45 +43,56 @@ const Chat = ({ senderId, receiverId, receiverName, roomId }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     if (inputMessage.trim() !== '') {
-      setIsLoading(true); // Set loading state to true when sending message
-      socket.emit('chat message', { message: inputMessage, senderId, roomId, receiverId });
       setInputMessage('');
-      setIsLoading(false); // Set loading state to false after message is sent
+      setIsLoading(true);
+  
+      socket.emit('chat message', { content: inputMessage, senderId, roomId, receiverId }, (ack) => {
+        if (ack && ack.success) {
+          setIsLoading(false); // Set loading state to false after message is sent successfully
+        } else {
+          // Handle error scenario if necessary
+          setIsLoading(false);
+        }
+      });
     }
   };
+  
+  
 
   return (
-    <Box>
-      <Text fontSize="xl" fontWeight="bold" textAlign="center" mb={4}>Chat</Text>
-      <List spacing={3} mb={4}>
-        {messages.map((message, index) => (
-          <ListItem
-            key={index}
-            textAlign={message.sender === senderId ? 'right' : 'left'}
-            fontSize="md"
-          >
-            {message.senderId === senderId ? 'You' : receiverName}: {message.message}
-          </ListItem>
-        ))}
-      </List>
-      <form onSubmit={handleSubmit}>
-        <Flex alignItems="center">
-          <Input
-            type="text"
-            value={inputMessage}
-            onChange={handleInputChange}
-            placeholder="Type your message..."
-            mr={2}
-          />
-          {/* Show spinner when loading, otherwise show send button */}
-          {isLoading ? (
-            <Spinner size="sm" />
-          ) : (
-            <Button colorScheme="blue" type="submit">Send</Button>
-          )}
-        </Flex>
-      </form>
+    <div>
+    <Box height="600px" overflowY="auto">
+    <Text fontSize="xl" fontWeight="bold" textAlign="center" mb={4}>Chat</Text>
+    <List spacing={3} mb={4}>
+      {messages.map((message, index) => (
+        <ListItem
+          key={index}
+          textAlign={message.senderId === senderId ? 'right' : 'left'}
+          fontSize="md"
+        >
+          <Box>{message.senderId === senderId ? 'You' : receiverName}<Box color="blue.300">{message.content}</Box><Box color='gray.300'>{(new Date(message.timestamp).toString() === 'Invalid Date') ? 'now' : new Date(message.timestamp).toLocaleString()}</Box></Box>
+        </ListItem>
+      ))}
+    </List>
     </Box>
+    <form onSubmit={handleSubmit} style={{ marginTop: "auto", marginBottom: "1rem" }}>
+      <Flex alignItems="center">
+        <Input
+          type="text"
+          value={inputMessage}
+          onChange={handleInputChange}
+          placeholder="Type your message..."
+          mr={2}
+        />
+        {isLoading ? (
+          <Spinner size="sm" />
+        ) : (
+          <Button colorScheme="blue" type="submit">Send</Button>
+        )}
+      </Flex>
+    </form>
+    </div>
+  
   );
 };
 
