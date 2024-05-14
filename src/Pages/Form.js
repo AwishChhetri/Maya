b8ab0013f4aaa347420890312a06d45c2f566054
ABISHCHHETRI <165697751+ABISHCHHETRI@users.noreply.github.com>
2024-05-14 10:56:59 +0530

    final

--- src/Pages/Form.js
+++ src/Pages/Form.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { Radio, RadioGroup, VStack, Button,Text, useRadioGroup,useRadio, Box,HStack} from '@chakra-ui/react';
+import { Radio, RadioGroup, VStack, Button, Text, useRadioGroup, useRadio, Box, HStack, CircularProgress } from '@chakra-ui/react';
 import axios from 'axios';
-import {toast} from 'react-toastify';
-import {useNavigate } from 'react-router-dom'
-
-
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom'
 
 function RadioCard({ children, isChecked, color, ...props }) {
   const { getInputProps, getCheckboxProps } = useRadio(props);
@@ -49,8 +47,6 @@ const Question = ({ question, options, selectedValue, onChange }) => {
 
   const group = getRootProps();
 
-  
-
   return (
     <VStack spacing={2} {...group}>
       <Text color="black" fontSize={18} fontWeight="bold">{question}</Text>
@@ -66,16 +62,16 @@ const Question = ({ question, options, selectedValue, onChange }) => {
   );
 };
 
-
-
 const Form = () => {
   const [selectedOptions, setSelectedOptions] = useState({});
   const [sex, setSex] = useState('');
   const [name, setName] = useState('');
   const [lookingFor, setLookingFor] = useState('');
   const [pickerStatus, setPickerStatus] = useState(null);
+  const [loading, setLoading] = useState(false);
   const userId = localStorage.getItem('_id');
-  const navigate=useNavigate()
+  const navigate = useNavigate();
+
   const questions = [
     {
       question: "What are your long-term goals beyond university, and how do you see a partner fitting into those plans?",
@@ -123,11 +119,11 @@ const Form = () => {
       key: 'question10'
     }
   ];
+
   useEffect(() => {
     // Fetch student details and Picker status
     axios.post('/studentDetails', { userId })
       .then(response => {
-        // console.log('Student details:', response.data);
         setPickerStatus(response.data.PickerStatus);
         setName(response.data.name)
       })
@@ -145,22 +141,23 @@ const Form = () => {
 
   const handleSubmit = async () => {
     try {
+      setLoading(true); // Set loading state to true
       // Check if all questions are answered
       const isAllQuestionsAnswered = questions.every(question => selectedOptions[question.key]);
       if (!isAllQuestionsAnswered || !sex || !lookingFor) {
         toast.warning('Please fill all the fields.');
+        setLoading(false); // Reset loading state
         return; // Exit early if any field is empty
       }
-  
+
       // Check if Picker status is false
       if (pickerStatus === true) {
-        toast.info("Form already submitted !",);
+        toast.info("Form already submitted !");
+        setLoading(false); // Reset loading state
         return;
       }
       const selectedValuesArray = Object.values(selectedOptions);
-      // If all fields are filled and Picker status is true, proceed with submission
-      // console.log('Selected options:', selectedValuesArray);
-  
+
       // Extract only the values from selectedOptions
       const selectedValues = {};
       for (const key in selectedOptions) {
@@ -168,109 +165,109 @@ const Form = () => {
           selectedValues[key] = selectedOptions[key].value;
         }
       }
-  
+
       // Send data to the server
       const response = await axios.post('/cupidPicker', {
         selectedOptions: selectedValuesArray, // Send only the values to the backend
         userId,
         sex,
-        lookingFor
+        lookingFor,
+        pickerStatus
       });
 
-      if(response.status==200){
+      if (response.status === 200) {
         navigate('/board')
-        toast.success("Your preference have been received!")
+        toast.success("Your preferences have been received!")
+      } else {
+        toast.error("Failed to submit!")
       }
-      else{
-        toast.error("Fail to submit!")
-      }
-  
-      // console.log('Server response:', response.data);
-      // Optionally, you can perform additional actions based on the server response
+
+      setLoading(false); // Reset loading state
     } catch (error) {
       console.error('Error sending data:', error);
-      // Handle error appropriately
+      toast.error("Failed to submit!")
+      setLoading(false); // Reset loading state
     }
   };
-  
 
   return (
     <VStack spacing={4} align="center" p={6}>
-    <Text color="black" fontWeight="bold" fontSize={20}>{name}</Text>
-  
-    {/* Display an inline message if Picker status is false */}
-    {pickerStatus === true && (
-      <Text color="red">Form already submitted.</Text>
-    )}
-  
-    {/* Render the form only if the test is not taken */}
-    {pickerStatus !== true && (
-      <div>
-        <VStack spacing={8}>
-          <HStack spacing={4}>
-          <Box
-              borderWidth="1px"
-              borderRadius="lg"
-              overflow="hidden"
-              mb={4}
-              p={4}
-              position="relative"
-              boxShadow="lg"
-            >
-            <Question
-              question="Your gender:"
-              options={["Male", "Female"]}
-              selectedValue={sex}
-              onChange={setSex}
-            /></Box>
-             <Box
-              borderWidth="1px"
-              borderRadius="lg"
-              overflow="hidden"
-              mb={4}
-              p={4}
-              position="relative"
-              boxShadow="lg"
-            >
-            <Question
-              question="You are looking for"
-              options={["Male", "Female"]}
-              selectedValue={lookingFor}
-              onChange={setLookingFor}
-            /></Box>
-          </HStack>
-  
-          <VStack spacing={4}>
-            {questions.map(({ question, options, key }) => (
-              
-              <Box
-              borderWidth="1px"
-              borderRadius="lg"
-              overflow="hidden"
-              mb={4}
-              p={4}
-              position="relative"
-              boxShadow="lg"
-            >
-              <Question
-                key={key}
-                question={question}
-                options={options}
-                selectedValue={selectedOptions[key]}
-                onChange={(value) => handleRadioChange(key, value)}
-              /></Box>
-            ))}
-          </VStack>
-        </VStack>
-  
-        <Button color="teal.600" size="lg" align="right" spacing={4} onClick={handleSubmit}>Submit</Button>
+      <Text color="black" fontWeight="bold" fontSize={20}>{name}</Text>
 
-      </div>
-    )}
-  </VStack>
-  
+      {/* Display an inline message if Picker status is false */}
+      {pickerStatus === true && (
+        <Text color="red">Form already submitted.</Text>
+      )}
+
+      {/* Render the form only if the test is not taken */}
+      {pickerStatus !== true && (
+        <div>
+          <VStack spacing={8}>
+            <HStack spacing={4}>
+              <Box
+                borderWidth="1px"
+                borderRadius="lg"
+                overflow="hidden"
+                mb={4}
+                p={4}
+                position="relative"
+                boxShadow="lg"
+              >
+                <Question
+                  question="Your gender:"
+                  options={["Male", "Female"]}
+                  selectedValue={sex}
+                  onChange={setSex}
+                />
+              </Box>
+              <Box
+                borderWidth="1px"
+                borderRadius="lg"
+                overflow="hidden"
+                mb={4}
+                p={4}
+                position="relative"
+                boxShadow="lg"
+              >
+                <Question
+                  question="You are looking for"
+                  options={["Male", "Female"]}
+                  selectedValue={lookingFor}
+                  onChange={setLookingFor}
+                />
+              </Box>
+            </HStack>
+
+            <VStack spacing={4}>
+              {questions.map(({ question, options, key }) => (
+                <Box
+                  borderWidth="1px"
+                  borderRadius="lg"
+                  overflow="hidden"
+                  mb={4}
+                  p={4}
+                  position="relative"
+                  boxShadow="lg"
+                >
+                  <Question
+                    key={key}
+                    question={question}
+                    options={options}
+                    selectedValue={selectedOptions[key]}
+                    onChange={(value) => handleRadioChange(key, value)}
+                  />
+                </Box>
+              ))}
+            </VStack>
+          </VStack>
+
+          <Button color="teal.600" size="lg" align="right" spacing={4} onClick={handleSubmit}>
+            {loading ? <CircularProgress isIndeterminate color="teal.600" size="24px" /> : "Submit"}
+          </Button>
+        </div>
+      )}
+    </VStack>
   );
 };
 
 export default Form;
-
