d0ec6d2860b56fc7676cd5f4d4928125a62ec57b
ABISHCHHETRI <165697751+ABISHCHHETRI@users.noreply.github.com>
2024-05-16 19:24:50 +0530

    new

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import axios from 'axios';
 import Chat from './Pages/Chat.js';
 import Form from './Pages/Form.js';
 import {PrivateRoute} from './Routes/PrivateRoute.js'
-//  axios.defaults.baseURL = 'http://cupidpuppy-env.eba-3rwswcn9.ap-south-1.elasticbeanstalk.com'; 
- axios.defaults.baseURL = 'http://localhost:8000';
+ axios.defaults.baseURL = 'http://cupidpuppy-env.eba-3rwswcn9.ap-south-1.elasticbeanstalk.com'; 
+//  axios.defaults.baseURL = 'http://localhost:8000';
 
 
 
